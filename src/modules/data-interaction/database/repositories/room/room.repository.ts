fef7de9c1e9f0082d81898b9a05d9ef79c26b3e1
Victor Andrade <0vetor0@gmail.com>
2023-12-20 13:22:12 -0300

    Fix import casing in room.repository.ts

--- src/modules/data-interaction/database/repositories/room/room.repository.ts
+++ src/modules/data-interaction/database/repositories/room/room.repository.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BaseRepository } from 'src/core/repositories/base.repository';
 import { Repository } from 'typeorm';
-import { CreateRoomDto } from '../../dtos/room/create-Room.dto';
-import { UpdateRoomDto } from '../../dtos/room/update-Room.dto';
-import { RoomEntity } from '../../entitites/Room.entity';
+import { CreateRoomDto } from '../../dtos/room/create-room.dto';
+import { UpdateRoomDto } from '../../dtos/room/update-room.dto';
+import { RoomEntity } from '../../entitites/room.entity';
 
 @Injectable()
 export class RoomRepository extends BaseRepository<RoomEntity, CreateRoomDto, UpdateRoomDto> {
