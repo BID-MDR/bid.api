59e9b5d97b275daabcc1a9bf872ff1401efd696a
Victor Andrade <0vetor0@gmail.com>
2024-02-06 11:54:31 -0300

    Add @aws-sdk/client-s3 dependency and update AWS S3 subsystem

--- src/modules/data-interaction/facade/apis/storage/aws.subsystem.ts
+++ src/modules/data-interaction/facade/apis/storage/aws.subsystem.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { S3 } from 'aws-sdk';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { EnviromentVariablesEnum } from 'src/core/enums/environment-variables.enum';
 
 @Injectable()
 export class AwsSubsystem {
-    s3Client: S3;
+    s3Client: S3Client;
 
     constructor(private readonly configService: ConfigService) {
-        this.s3Client = new S3({
+        this.s3Client = new S3Client({
             region: this.configService.get(EnviromentVariablesEnum.AWS_BUCKET_REGION),
             credentials: {
                 accessKeyId: configService.get(EnviromentVariablesEnum.AWS_BUCKET_ACCESS_KEY_ID),
@@ -23,16 +23,20 @@ export class AwsSubsystem {
             file = Buffer.from(file.split(';base64,').pop(), 'base64');
         }
 
-        return (
-            await this.s3Client
-                .upload({
-                    Bucket: this.configService.get(EnviromentVariablesEnum.AWS_BUCKET_NAME),
-                    Key: fileName,
-                    Body: file,
-                    ContentType: fileMimeType,
-                    ACL: 'public-read',
-                })
-                .promise()
-        ).Location;
+        await this.s3Client.send(
+            new PutObjectCommand({
+                Bucket: this.configService.get(EnviromentVariablesEnum.AWS_BUCKET_NAME),
+                Key: fileName,
+                Body: file,
+                ContentType: fileMimeType,
+                ACL: 'public-read',
+            }),
+        );
+
+        return encodeURI(
+            `https://${this.configService.get(EnviromentVariablesEnum.AWS_BUCKET_NAME)}.s3.${this.configService.get(
+                EnviromentVariablesEnum.AWS_BUCKET_REGION,
+            )}.amazonaws.com/${fileName}`,
+        );
     }
 }
