7124c64698bf1cd13897945b6c97e5f2c0b82e81
vitucodetech <163009631+vitucodetech@users.noreply.github.com>
2024-03-22 04:06:18 -0300

    Update tabela Estimativa de custo

--- src/modules/data-interaction/database/dtos/cost-estimation/create-cost-estimation.dto.ts
+++ src/modules/data-interaction/database/dtos/cost-estimation/create-cost-estimation.dto.ts
@@ -1,24 +1,31 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsUUID, ValidateNested } from 'class-validator';
+import { IsEnum, IsUUID, Length, ValidateNested } from 'class-validator';
 import { UserEntity } from '../../entitites/user.entity';
 import { WorkRequestEntity } from '../../entitites/work-request.entity';
 import { CreateRoomSolutionDto } from '../room-solution/create-room-solution.dto';
 import { Type } from 'class-transformer';
+import { CostEstimationStatusEnum } from '../../enums/cost-estimation-status.enum';
+import { ProfessionalDeclineResponseEnum } from '../../enums/professional-decline-response.enum';
 
 export class CreateCostEstimationDto {
-    professionalId: string;
-    professional: UserEntity;
+    @ApiProperty({ enum: CostEstimationStatusEnum })
+    @IsEnum(CostEstimationStatusEnum)
+    status: CostEstimationStatusEnum;
+
+    @ApiProperty({ enum: ProfessionalDeclineResponseEnum })
+    @IsEnum(ProfessionalDeclineResponseEnum)
+    professionalDeclineResponse: ProfessionalDeclineResponseEnum;
 
     @ApiProperty()
-    @IsUUID()
-    workRequestId: string;
-    workRequest: WorkRequestEntity;
+    @Length(1, 70)
+    beneficiaryAdjustmentResponse: string;
 
-    @ApiProperty({ type: CreateRoomSolutionDto, isArray: true })
-    @ValidateNested({ each: true })
-    @Type(() => CreateRoomSolutionDto)
-    roomsSolutions: CreateRoomSolutionDto[];
-
-    @ApiProperty({ description: 'Tempo estimado para execução em dias' })
+    @ApiProperty()
     estimatedTimeToExecute: number;
+
+    @ApiProperty()
+    professionalId: string;
+
+    @ApiProperty()
+    workRequestId: string;
 }
--- src/modules/data-interaction/database/dtos/cost-estimation/update-cost-estimation.dto.ts
+++ src/modules/data-interaction/database/dtos/cost-estimation/update-cost-estimation.dto.ts
@@ -2,12 +2,12 @@ import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, ValidateNested } from 'class-validator';
 import { UpdateRoomSolutionDto } from '../room-solution/update-room-solution.dto';
+import { DeepPartial } from 'typeorm';
 import { CreateCostEstimationDto } from './create-cost-estimation.dto';
+import { CostEstimationEntity } from '../../entitites/cost-estimation.entity';
 
-export class UpdateCostEstimationDto extends OmitType(PartialType(CreateCostEstimationDto), ['roomsSolutions']) {
-    @ApiProperty({ type: UpdateRoomSolutionDto, isArray: true })
-    @ValidateNested({ each: true })
-    @Type(() => UpdateRoomSolutionDto)
+export class UpdateCostEstimationDto extends PartialType(CreateCostEstimationDto) {
+    @ApiProperty({ required: false })
     @IsOptional()
-    roomsSolutions: UpdateRoomSolutionDto[];
+    id?: string;
 }
