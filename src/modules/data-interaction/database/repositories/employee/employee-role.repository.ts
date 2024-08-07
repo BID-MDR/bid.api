d7880dec0e4b1a55db7b48f50bba7c7e8331c2a6
gustavo lorenz <gustavolorenz43@gmail.com>
2024-08-06 21:36:28 -0300

    BID-371

--- src/modules/data-interaction/database/repositories/employee/employee-role.repository.ts
+++ src/modules/data-interaction/database/repositories/employee/employee-role.repository.ts
@@ -3,9 +3,10 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { BaseRepository } from "../../../../../core/repositories/base.repository";
 import { EmployeeRoleEntity } from "../../entitites/employee-role.entity";
+import { CreateEmployeeRoleDto } from "../../dtos/employee-role/employee-role-create.dto";
 
 @Injectable()
-export class EmployeeRoleRepository extends BaseRepository<EmployeeRoleEntity, any, any> {
+export class EmployeeRoleRepository extends BaseRepository<EmployeeRoleEntity, CreateEmployeeRoleDto, CreateEmployeeRoleDto> {
   constructor(@InjectRepository(EmployeeRoleEntity) private repository: Repository<EmployeeRoleEntity>) {
     super(repository);
   }
