30f3593abbd7f8b67c96610b412c8e619b76e046
gustavo lorenz <gustavolorenz43@gmail.com>
2025-01-08 20:42:47 -0300

    Ajusta gets com as relacoes adiciona metodos faltantes

---
+++ src/modules/data-interaction/database/dtos/cost-estimate/cost-estimate-aprove-reprove-request.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+import {IsEnum} from "class-validator";
+import { CostEstimateStatusEnum } from "../../enums/cost-estimate-status.enum";
+
+
+export class CostEstimateAproveReproveRequestDto {
+    @ApiProperty({ enum: CostEstimateStatusEnum })
+    @IsEnum(CostEstimateStatusEnum)
+    type: CostEstimateStatusEnum;
+}
