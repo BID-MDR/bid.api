19beaf49a2871c095f60cf05b4fd2f8e4e76ab97
johnshkim <jshk@hotmail.com>
2025-01-14 09:04:19 -0300

    Update update-technical-visit.dto.ts

--- src/modules/data-interaction/database/dtos/technical-visit/update-technical-visit.dto.ts
+++ src/modules/data-interaction/database/dtos/technical-visit/update-technical-visit.dto.ts
@@ -1,7 +1,8 @@
 import { ApiProperty, PartialType, PickType } from '@nestjs/swagger';
 import { CreateTechnicalVisitDto } from './create-technical-visit.dto';
-import { IsEnum, Length } from 'class-validator';
+import { IsEnum, IsUUID, Length } from 'class-validator';
 import { TechnicalVisitStatusEnum } from '../../enums/technical-visit-status.enum';
+import { UserEntity } from '../../entitites/user.entity';
 
 export class UpdateTechnicalVisitDto extends PickType(PartialType(CreateTechnicalVisitDto), ['to', 'from']) {
     @ApiProperty()
@@ -11,4 +12,9 @@ export class UpdateTechnicalVisitDto extends PickType(PartialType(CreateTechnica
     @ApiProperty()
     @IsEnum(TechnicalVisitStatusEnum)
     status: TechnicalVisitStatusEnum;
+
+    @ApiProperty()
+    @IsUUID()
+    professionalId: string;
+    professional: UserEntity;
 }
