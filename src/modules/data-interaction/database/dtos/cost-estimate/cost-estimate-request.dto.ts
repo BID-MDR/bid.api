9d8325bf0dbcc5eaf2c4f3ed764a89e9040b300a
Luiz Eduardo Satélis dos Santos <edusatelis@gmail.com>
2025-01-14 17:43:48 -0300

    Adicionando professional ID na tabela

--- src/modules/data-interaction/database/dtos/cost-estimate/cost-estimate-request.dto.ts
+++ src/modules/data-interaction/database/dtos/cost-estimate/cost-estimate-request.dto.ts
@@ -2,6 +2,7 @@ import { ApiProperty } from "@nestjs/swagger";
 import {IsString} from "class-validator";
 import { RoomEntity } from "../../entitites/room.entity";
 import { WorkRequestEntity } from "../../entitites/work-request.entity";
+import { UserEntity } from "../../entitites/user.entity";
 
 
 export class CreateCostEstimateRequestDto {
@@ -21,5 +22,10 @@ export class CreateCostEstimateRequestDto {
 
     @ApiProperty()
     estimateDate: string
+
+    @ApiProperty()
+    professionalId?: string;
+
+    professional: UserEntity
 
 }
