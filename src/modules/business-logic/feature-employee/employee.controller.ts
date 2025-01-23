3182adb45d9623db1e40479806352f11c91d555b
Luiz Eduardo Satélis dos Santos <edusatelis@gmail.com>
2025-01-23 15:09:54 -0300

    Ajuste update role

--- src/modules/business-logic/feature-employee/employee.controller.ts
+++ src/modules/business-logic/feature-employee/employee.controller.ts
@@ -12,12 +12,10 @@ export class EmployeeController {
   private readonly _logger = new Logger(EmployeeController.name);
   constructor(private service: EmployeeService) {}
 
-  @Post("register")
-  @ApiBearerAuth()
-  @UseGuards(JwtAccessTokenGuard)
-  async register(@Body() dto: EmployeeRegisterRequestDto, @Req() req: Request) {
-    const userId = (req.user as JwtPayloadInterface).userId;
-    return await this.service.register(dto, userId);
+  @Post("register/:id")
+  async register(@Param('id') id:string,  @Body() dto: EmployeeRegisterRequestDto) {
+    console.log(id)
+    return await this.service.register(dto, id);
   }
 
   @Put("active/:id")
@@ -28,11 +26,45 @@ export class EmployeeController {
     return await this.service.activeEmployee(id, userId);
   }
 
+  @Put("role/:id")
+  @ApiBearerAuth()
+  @UseGuards(JwtAccessTokenGuard)
+  async updateRoleEmployee(@Param("id") id: string,@Body() roleId: any ,@Req() req: Request) {
+    const userId = (req.user as JwtPayloadInterface).userId;
+    return await this.service.updateRole(id,roleId, userId);
+  }
+
+  @Put("role-aproved/:id")
+  @ApiBearerAuth()
+  @UseGuards(JwtAccessTokenGuard)
+  async updateRoleEmployeeAprove(@Param("id") id: string,@Body() roleId: any ,@Req() req: Request) {
+    const userId = (req.user as JwtPayloadInterface).userId;
+    return await this.service.updateRoleAndActive(id,roleId, userId);
+  }
+
+  @Put("role-reject/:id")
+  @ApiBearerAuth()
+  @UseGuards(JwtAccessTokenGuard)
+  async updateRoleEmployeeReject(@Param("id") id: string,@Body() roleId: any ,@Req() req: Request) {
+    const userId = (req.user as JwtPayloadInterface).userId;
+    return await this.service.updateRoleAndReject(id,roleId, userId);
+  }
+
   @Get("")
   @ApiBearerAuth()
   @UseGuards(JwtAccessTokenGuard)
   async list() {
     return await this.service.list();
+  }
+
+  @Get("by-company")
+  @ApiBearerAuth()
+  @UseGuards(JwtAccessTokenGuard)
+  async listByCompany(@Req() req: Request) {
+
+    const companyId = (req.user as JwtPayloadInterface).companyId;
+
+    return await this.service.listByCompany(companyId);
   }
 
 }
