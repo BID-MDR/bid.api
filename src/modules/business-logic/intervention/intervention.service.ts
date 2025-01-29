74ba4b5970591b6fef8ac435c1d2368c8e530234
Matheus Rodrigues de Oliveira <matheusrdeoliv1@gmail.com>
2025-01-29 13:24:43 -0300

    Update intervention.service.ts

--- src/modules/business-logic/intervention/intervention.service.ts
+++ src/modules/business-logic/intervention/intervention.service.ts
@@ -1,10 +1,9 @@
-import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
-import { BaseService } from "../../../core/services/base.service";
-import { CreateWorkRequestDto } from "../../data-interaction/database/dtos/work-request/create-work-request.dto";
-import { InterventionEntity } from "src/modules/data-interaction/database/entitites/intervention.entity";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateInterventionRequestDto } from "src/modules/data-interaction/database/dtos/intervention/intervention-request.dto";
+import { InterventionEntity } from "src/modules/data-interaction/database/entitites/intervention.entity";
 import { InterventionRepository } from "src/modules/data-interaction/database/repositories/intervention/intervention.repository";
 import { RoomRepository } from "src/modules/data-interaction/database/repositories/room/room.repository";
+import { BaseService } from "../../../core/services/base.service";
 
 @Injectable()
 export class InterventionService extends BaseService<InterventionEntity, CreateInterventionRequestDto, CreateInterventionRequestDto> {
@@ -19,7 +18,7 @@ export class InterventionService extends BaseService<InterventionEntity, CreateI
   }
 
   async list() {
-    const list =  await this.repository.findAll();
+    const list = await this.repository.findAll();
     console.log('lis', list)
     return list
   }
@@ -36,26 +35,24 @@ export class InterventionService extends BaseService<InterventionEntity, CreateI
     return await this.repository.create(data)
   }
 
- 
-
   async update(interventionId: string, data: CreateInterventionRequestDto) {
     const intervention = await this.repository.findById(interventionId)
-    if(!intervention) throw new NotFoundException('Intervention not found')
+    if (!intervention) throw new NotFoundException('Intervention not found')
     if (data.roomId) {
       const room = await this.roomRepo.findById(data.roomId)
-      if(!room) throw new NotFoundException('Room not found!')
+      if (!room) throw new NotFoundException('Room not found!')
       data.room = room
       delete data.roomId
     }
-    if(data.step) {
-      const currentStep  = intervention.step
+    if (data.step) {
+      const currentStep = intervention.step;
       if (!currentStep.split(',').includes(data.step)) {
         data.step = currentStep ? `${currentStep},${data.step}` : data.step;
-    } else {
+      } else {
         data.step = currentStep;
+      }
     }
 
-    }
     return await super.update(interventionId, data);
   }
 
