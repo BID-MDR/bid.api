8ab718043101e3f90cb60ff3e7fe493b0e3388a9
gustavo lorenz <gustavolorenz43@gmail.com>
2025-02-03 17:19:22 -0300

    ajusta cadastro de obra

--- src/modules/data-interaction/database/repositories/user/demand.repository.ts
+++ src/modules/data-interaction/database/repositories/user/demand.repository.ts
@@ -29,6 +29,18 @@ export class DemandRepository extends BaseRepository<
     return query.getOne();
 }
 
+async getById2(_id: string) {
+ return this.repository
+  .createQueryBuilder('demand')
+  .leftJoinAndSelect('demand.company', 'company')
+  .leftJoinAndSelect('demand.workRequest', 'workRequest')
+  .leftJoinAndSelect('workRequest.room', 'room')
+  .leftJoinAndSelect('room.roomSolutions', 'room_solution')
+  .where("demand.id = :id", { id: _id })
+  .getOne()
+}
+
+
   async countSustainabilityItems(document: string | number) {
     const count = await this.repository
       .createQueryBuilder("demand")
