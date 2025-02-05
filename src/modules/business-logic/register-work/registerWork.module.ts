b2527a67f6712eaa5f3c194698fe06b84f1da82e
gustavo lorenz <gustavolorenz43@gmail.com>
2025-02-04 21:56:31 -0300

    finaliza cadastro de obra

--- src/modules/business-logic/register-work/registerWork.module.ts
+++ src/modules/business-logic/register-work/registerWork.module.ts
@@ -5,9 +5,10 @@ import { FeatureAuthModule } from "../feature-auth/feature-auth.module";
 import {  RegisterWorkService } from "./registerWork.service";
 import {  RegisterWorkController } from "./registerWork.controller";
 import { NotificationMessageModule } from "../notification-msg/notification-message.module";
+import { BidDocumentModule } from "../bid-document/bidDocument.module";
 
 @Module({
-    imports: [DatabaseModule, FacadeModule, FeatureAuthModule, NotificationMessageModule],
+    imports: [DatabaseModule, FacadeModule, FeatureAuthModule, NotificationMessageModule, BidDocumentModule],
     providers: [RegisterWorkService],
     controllers: [RegisterWorkController],
 })
