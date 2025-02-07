5af8e8ee92564174873fa4dfdfd3390d426daa95
johnshkim <jshk@hotmail.com>
2025-02-07 03:20:03 -0300

    Update update-work-request.dto.ts

--- src/modules/data-interaction/database/dtos/work-request/update-work-request.dto.ts
+++ src/modules/data-interaction/database/dtos/work-request/update-work-request.dto.ts
@@ -6,10 +6,13 @@ import {
     Max,
     Min,
     IsEnum,
+    ValidateNested,
 } from "class-validator";
 import { FlooringEnum } from "../../enums/flooring.enum";
 import { KinshipEnum } from "../../enums/kinship.enum";
 import { PropertyTypeEnum } from "../../enums/property-type.enum";
+import { MediaUploadDto } from "../media/media-upload.dto";
+import { Type } from "class-transformer";
 
 export class UpdateWorkRequestDto {
     @ApiProperty()
@@ -38,4 +41,11 @@ export class UpdateWorkRequestDto {
     @IsNotEmpty()
     @IsEnum(FlooringEnum)
     flooring: FlooringEnum;
+
+    @ApiProperty({ type: [MediaUploadDto] })
+    @ValidateNested()
+    @Type(() => MediaUploadDto)
+    selectedFiles?: MediaUploadDto[];
+
+    pictures?: string[];
 }
