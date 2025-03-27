12fca8e62704c3cc84e53fe1343d6b3b22e12f1b
johnshkim <jshk@hotmail.com>
2025-03-27 19:03:26 -0300

    update

--- src/modules/data-interaction/facade/apis/storage/storage.module.ts
+++ src/modules/data-interaction/facade/apis/storage/storage.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AwsSubsystem } from './aws.subsystem';
 import { StorageFacade } from './storage.facade';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+    imports: [HttpModule],
     providers: [AwsSubsystem, StorageFacade],
     exports: [AwsSubsystem, StorageFacade],
 })
+
 export class StorageModule {}
