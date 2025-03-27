3c2cd43fe68f442c6dd07c782938d2e97ac246e0
johnshkim <jshk@hotmail.com>
2025-03-27 17:51:15 -0300

    Update aws.subsystem.ts

--- src/modules/data-interaction/facade/apis/storage/aws.subsystem.ts
+++ src/modules/data-interaction/facade/apis/storage/aws.subsystem.ts
@@ -39,43 +39,51 @@ export class AwsSubsystem {
 
   async uploadMedia(fileMimeType: string, fileName: string, file: Buffer | string) {
     this.validateConnection();
+
     console.log('dentro do upload media');
-    console.log('fileMimeType',fileMimeType);
+    console.log('fileMimeType', fileMimeType);
     console.log('fileName', fileName);
     console.log('file', file);
+    this.s3Client = new S3Client({
+      region: 'sa-saopaulo-1',
+      credentials: {
+        accessKeyId: 'b66919a3701f48073ff4f1ca8002bf2ec709ec81',
+        secretAccessKey: '4WHlfgog0oZI65gWbqZD5mQqAOhAvs9FHlfYVsiy76s=',
+      },
+      endpoint: 'https://grumzjujmpu4.compat.objectstorage.sa-saopaulo-1.oraclecloud.com',
+      forcePathStyle: true,
+    });
     if (typeof file === "string" && file.includes(";base64,")) {
       console.log("Dentro do if, convertendo base64 para Buffer...");
       try {
-          const base64String = file.split(";base64,").pop();
-          if (!base64String) {
-              throw new Error("Base64 inválido");
-          }
-          file = Buffer.from(base64String, "base64");
-          console.log("Conversão bem-sucedida!", file);
+        const base64String = file.split(";base64,").pop();
+        if (!base64String) {
+          throw new Error("Base64 inválido");
+        }
+        file = Buffer.from(base64String, "base64");
+        console.log("Conversão bem-sucedida!", file);
       } catch (error) {
-          console.error("Erro ao converter base64:", error);
+        console.error("Erro ao converter base64:", error);
       }
-  }
-    console.log('fora do if ');
+      const result = await this.s3Client.send(
+        new PutObjectCommand({
+          Bucket: 'code-s3-001',
+          Key: fileName,
+          Body: file,
+          ContentType: fileMimeType,
+          ACL: "public-read",
+        })
+      );
 
-   
-    const result = await this.s3Client.send(
-      new PutObjectCommand({
-        Bucket: 'code-s3-001',
-        Key: fileName,
-        Body: file,
-        ContentType: fileMimeType,
-        ACL: "public-read",
-      })
-    );
+      console.log('result', result);
 
-    console.log('result',result);
-
-    if (result.$metadata.httpStatusCode !== 200) throw new Error('Error uploading file')
+      if (result.$metadata.httpStatusCode !== 200) throw new Error('Error uploading file')
 
       const localtion = `https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grumzjujmpu4/b/code-s3-001/o/${fileName}`;
-  
+
       return localtion;
+    }
+    console.log('fora do if ');
 
   }
 
