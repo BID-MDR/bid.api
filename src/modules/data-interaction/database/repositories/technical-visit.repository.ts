1028772891538f732cef911dc32ebaae1c7a2d9b
Caio Cardoso <89560756+Mantalaio@users.noreply.github.com>
2025-06-25 10:55:47 -0300

    Update technical-visit.repository.ts

--- src/modules/data-interaction/database/repositories/technical-visit.repository.ts
+++ src/modules/data-interaction/database/repositories/technical-visit.repository.ts
@@ -64,6 +64,7 @@ export class TechnicalVisitRepository extends BaseRepository<
         const relations = [
             'professional',
             'beneficiary',
+            'beneficiary.address',
             'demand',
             'workRequest',
             'workRequest.welfare',
