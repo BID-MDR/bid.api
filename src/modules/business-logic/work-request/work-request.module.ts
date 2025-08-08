05805410670844fae84e792f3f47a09a6d9b6ebf
bruno-bogochvol <147097793+bruno-bogochvol@users.noreply.github.com>
2025-08-08 14:54:49 -0300

    feat: import notification module

--- src/modules/business-logic/work-request/work-request.module.ts
+++ src/modules/business-logic/work-request/work-request.module.ts
@@ -6,9 +6,16 @@ import { WorkRequestService } from "./work-request.service";
 import { WorkRequestController } from "./work-request.controller";
 import { CostEstimateService } from "../cost-estimate/costEstimate.service";
 import { CostEstimateModule } from "../cost-estimate/costEstimate.module";
+import { NotificationMessageModule } from "../notification-msg/notification-message.module";
 
 @Module({
-    imports: [DatabaseModule, FacadeModule, FeatureAuthModule, CostEstimateModule],
+    imports: [
+        DatabaseModule,
+        FacadeModule,
+        FeatureAuthModule,
+        CostEstimateModule,
+        NotificationMessageModule,
+    ],
     providers: [WorkRequestService],
     controllers: [WorkRequestController],
 })
