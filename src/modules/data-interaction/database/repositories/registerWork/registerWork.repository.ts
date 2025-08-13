b53d89d5416362f2d3267163d3f1354b0e0c0677
Caio Cardoso <89560756+Mantalaio@users.noreply.github.com>
2025-08-13 13:54:33 -0300

    Enhance DashboardService and repositories with new methods for contract, cost estimate, improvement project, and register work management; refactor existing methods for improved readability and consistency.

--- src/modules/data-interaction/database/repositories/registerWork/registerWork.repository.ts
+++ src/modules/data-interaction/database/repositories/registerWork/registerWork.repository.ts
@@ -7,6 +7,7 @@ import { RegisterWorkCreateDto } from "../../dtos/register-work/register-work.dt
 import { RegisterWorkStatusEnum } from "../../enums/register-work.enum";
 import { ConstructionsStatusEnum } from "../../enums/constructions-stauts.enum";
 import { ConstructionsTypeEnum } from "../../enums/constructions-type.status";
+import { addMonths } from "date-fns";
 
 @Injectable()
 export class RegisterWorkRepository extends BaseRepository<
@@ -24,7 +25,7 @@ export class RegisterWorkRepository extends BaseRepository<
   async findById(costEstimateId: string): Promise<RegisterWorkEntity> {
     return await this.repository.findOne({
       where: { id: costEstimateId },
-     relations: [ 'professional', 'workRequest.contractResignedList', 'workRequest.beneficiary','workRequest.beneficiary.address', 'workRequest.room', 'workRequest.contracts'],
+      relations: ['professional', 'workRequest.contractResignedList', 'workRequest.beneficiary', 'workRequest.beneficiary.address', 'workRequest.room', 'workRequest.contracts'],
     });
   }
   async getByWorkRequestId(workRequestId: string): Promise<RegisterWorkEntity> {
@@ -45,9 +46,9 @@ export class RegisterWorkRepository extends BaseRepository<
       },
       relations: ['professional', 'workRequest', 'workRequest.beneficiary', 'workRequest.contractResignedList'],
     });
-  } 
-  
-  async getByProfessional(professionalId: string){
+  }
+
+  async getByProfessional(professionalId: string) {
     return this.repository
       .createQueryBuilder('registerWork')
       .leftJoinAndSelect('registerWork.workRequest', 'workRequest')
@@ -61,7 +62,7 @@ export class RegisterWorkRepository extends BaseRepository<
       .getMany();
   }
   async getByBeneficary(beneficaryId: string) {
-  
+
     return this.repository
       .createQueryBuilder('registerWork')
       .leftJoinAndSelect('registerWork.workRequest', 'workRequest')
@@ -70,35 +71,73 @@ export class RegisterWorkRepository extends BaseRepository<
       .leftJoinAndSelect('workRequest.technicalVisit', 'technicalVisit')
       .leftJoinAndSelect('workRequest.contracts', 'contract')
       .leftJoinAndSelect('workRequest.contractResignedList', 'contractResignedList')
-      .leftJoinAndSelect('workRequest.beneficiary', 'beneficiary') 
-      .where('beneficiary.id = :beneficaryId', { beneficaryId }) 
+      .leftJoinAndSelect('workRequest.beneficiary', 'beneficiary')
+      .where('beneficiary.id = :beneficaryId', { beneficaryId })
       .getMany();
   }
-  
-  async startRegisterWork(registerWorkId: string, ) {
-    return await this.repository.update({ id: registerWorkId }, {startedDate: new Date(), status: ConstructionsStatusEnum.EM_ANDAMENTO});
+
+  async startRegisterWork(registerWorkId: string,) {
+    return await this.repository.update({ id: registerWorkId }, { startedDate: new Date(), status: ConstructionsStatusEnum.EM_ANDAMENTO });
   }
   async finishRegisterWork(registerWorkId: string, description: string) {
-    return await this.repository.update({ id: registerWorkId }, { status: ConstructionsStatusEnum.CONCLUDED, description: description});
+    return await this.repository.update({ id: registerWorkId }, { status: ConstructionsStatusEnum.CONCLUDED, description: description });
   }
-  async updateStatus(registerWorkId: string, statusUpdate:ConstructionsStatusEnum ) {
-    return await this.repository.update({ id: registerWorkId }, { status: statusUpdate});
+  async updateStatus(registerWorkId: string, statusUpdate: ConstructionsStatusEnum) {
+    return await this.repository.update({ id: registerWorkId }, { status: statusUpdate });
   }
-  async updateTypeAreaDesc(registerWorkId: string, typeItem:ConstructionsTypeEnum, area: number, descr: string) {
-    return await this.repository.update({ id: registerWorkId }, { type: typeItem, area: area, description: descr, status:ConstructionsStatusEnum.TO_SCHEDULE_CONCLUSION, startedDate: new Date()});
+  async updateTypeAreaDesc(registerWorkId: string, typeItem: ConstructionsTypeEnum, area: number, descr: string) {
+    return await this.repository.update({ id: registerWorkId }, { type: typeItem, area: area, description: descr, status: ConstructionsStatusEnum.TO_SCHEDULE_CONCLUSION, startedDate: new Date() });
   }
 
-async concludedRegisterWork(registerWorkId: string): Promise<RegisterWorkEntity> {
-  const reg = await this.repository.findOne({ where: { id: registerWorkId } });
+  async concludedRegisterWork(registerWorkId: string): Promise<RegisterWorkEntity> {
+    const reg = await this.repository.findOne({ where: { id: registerWorkId } });
 
 
-  reg.concludedDate = new Date();
-  reg.status = ConstructionsStatusEnum.CONCLUDED;
+    reg.concludedDate = new Date();
+    reg.status = ConstructionsStatusEnum.CONCLUDED;
 
-  return this.repository.save(reg);
-}
-  async endRegisterWork(registerWorkId: string, ) {
-    return await this.repository.update({ id: registerWorkId }, {concludedDate: new Date(), status: ConstructionsStatusEnum.CONCLUDED});
+    return this.repository.save(reg);
+  }
+  async endRegisterWork(registerWorkId: string,) {
+    return await this.repository.update({ id: registerWorkId }, { concludedDate: new Date(), status: ConstructionsStatusEnum.CONCLUDED });
+  }
+
+  async countRegisterWork() {
+    const count = await this.repository.createQueryBuilder('registerWork')
+      .where('registerWork.status NOT IN (:...status)', {
+        status: [ConstructionsStatusEnum.CONCLUDED],
+      })
+      .select('COUNT(registerWork.id)', 'count')
+      .getRawOne();
+
+    return Number(count.count)
+  }
+
+  async countRegisterWorkConcluded() {
+    const count = await this.repository.createQueryBuilder('registerWork')
+      .where('registerWork.status IN (:...status)', {
+        status: [ConstructionsStatusEnum.CONCLUDED],
+      })
+      .select('COUNT(registerWork.id)', 'count')
+      .getRawOne();
+
+    return Number(count.count)
+  }
+
+  async findMonth(month?: number) {
+    const now = new Date();
+
+    const query = this.repository.createQueryBuilder('registerWork');
+
+    if (month && month > 0) {
+      const pastDate = addMonths(now, -month);
+      query.where('registerWork.createdAt BETWEEN :pastDate AND :now', {
+        pastDate: pastDate.toISOString(),
+        now: now.toISOString(),
+      });
+    }
+
+    return query.getMany();
   }
 
 }
